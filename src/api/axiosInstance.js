eb609b51bfafdf05763eb6878565ee817bef3567
Prakash Mishra <pm2466599@gmail.com>
2025-11-08 09:14:42 +0530

    initial

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000",
+  baseURL: "https://google-auth-backend-vaei.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
